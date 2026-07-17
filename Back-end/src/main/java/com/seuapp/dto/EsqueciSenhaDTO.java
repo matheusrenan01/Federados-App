cd80505f382cbe2ae32e158055e7b1d0290f2d5a
Rodrigo Limeira <rodrigolimeira2002@outlook.com>
2026-07-17 19:32:26 -0300

    Delete Back-end directory

--- Back-end/src/main/java/com/seuapp/dto/EsqueciSenhaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.seuapp.dto;
-
-public class EsqueciSenhaDTO {
-    private String email;
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-}
